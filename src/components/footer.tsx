52e9a4ec2cb912c9bf8b70ce843258c4f6d9e605
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-03-18 23:19:53 -0400

    feat: removed all components and pages

--- src/components/footer.tsx
+++
@@ -1,7 +0,0 @@
-export const Footer = () => {
-  return (
-    <footer className="bg-gray-600	col-span-6">
-      Todos os direitos reservados @2022
-    </footer>
-  );
-};
--- src/services/DateFormat.ts
+++
@@ -1,4 +0,0 @@
-export const DateFormat = (dateTime) => {
-  const date = new Date(dateTime.toString());
-  return new Intl.DateTimeFormat('pt-BR', { timeZone: 'UTC' }).format(date);
-};
